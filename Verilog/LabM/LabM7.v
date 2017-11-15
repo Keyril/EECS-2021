cdce56395aa3987440e25b6b9e7f7ebb297592c1
Keyril <kkresling@yahoo.ca>
2017-11-15 18:36:43 -0500

    adding LabM

---
+++ Verilog/LabM/LabM7.v
@@ -0,0 +1,31 @@
+module labM7;
+   reg [31:0] PCin;
+   reg        clk;
+   wire [31:0] ins, PCp4;
+   yIF myIF(ins, PCp4, PCin, clk);
+
+   initial
+     begin
+        //Entry point
+        PCin = 128;
+
+        //Run program
+
+        repeat (11)
+          begin
+             //Fetch an instance
+
+             clk = 1; #1;
+
+             //Execute the ins
+
+             clk = 0; #1;
+
+             //View results
+
+             $display("instruction = %h", ins);
+             PCin = PCp4;
+          end
+        $finish;
+     end
+endmodule //LabM7
---
+++ Verilog/LabM/register.v
@@ -0,0 +1,13 @@
+module register (z, d, clk, enable);
+
+input [31:0] z;
+input [31:0] d;
+input clk;
+input enable;
+
+
+
+
+
+
+endmodule
